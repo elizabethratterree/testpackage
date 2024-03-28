155429881f0580876d07a8a7a304acaf190cb316
Elizabeth_Ratterree <elizabeth.ratterree@my.utsa.edu>
2024-03-28 12:47:35 -0500

    test 8

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ setup(
     name='testpackage',
     version='1.0.0',
     packages=find_packages(),
+    package_data={'testpackage': ['source/testpackage/*']},
     url='https://github.com/elizabethratterree/testpackage',
-    dependency_links=[
-        'git+https://github.com/elizabethratterree/testpackage.git#egg=testpackage-1.0.0'
-    ],
     install_requires=[
-        # List your dependencies here
+        'testpackage @ git+https://github.com/elizabethratterree/testpackage.git'
     ],
 )
