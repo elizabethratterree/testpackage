5b22f6c82ecedc2753d53ed4727a99d19644f29c
Elizabeth_Ratterree <elizabeth.ratterree@my.utsa.edu>
2024-03-28 13:09:31 -0500

    test 9

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='testpackage',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
-    package_data={'testpackage': ['source/testpackage/*']},
     url='https://github.com/elizabethratterree/testpackage',
     install_requires=[
         'testpackage @ git+https://github.com/elizabethratterree/testpackage.git'
