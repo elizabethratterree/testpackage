aee61a77f0a548f6ceedf2992d0c35b104759855
elizabethratterree <164931800+elizabethratterree@users.noreply.github.com>
2024-03-28 12:32:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     url='https://github.com/elizabethratterree/testpackage',
-    dependency_links=[
-        'git+https://github.com/elizabethratterree/testpackage.git#egg=testpackage-1.0.0'
-    ],
     install_requires=[
         # List your dependencies here
     ],
