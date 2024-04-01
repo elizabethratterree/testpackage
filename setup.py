cfc980369d965982511644a932e2be151fe89d11
Elizabeth_Ratterree <elizabeth.ratterree@my.utsa.edu>
2024-04-01 10:04:18 -0500

    test10

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
-def run_setup():
-    setup(
-        name='testpackage',
-        version='1.0.1',
-        download_url='https://github.com/elizabethratterree/testpackage',
-    )
+from testpackage1 import __version__
+
+setup(
+    name='testpackage1',
+    version=__version__,
+
+    url='https://github.com/elizabethratterree/testpackage',
+    author='Elizabeth Ratterree',
+    author_email='elizabeth.ratterree@my.utsa.edu',
+
+    py_modules=['testpackage1'],
+)
