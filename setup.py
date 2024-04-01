0757cf9c595784271fa737fc884d571f1b41515c
elizabethratterree <164931800+elizabethratterree@users.noreply.github.com>
2024-04-01 09:33:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ def run_setup():
     setup(
         name='testpackage',
         version='1.0.1',
-        packages=['source','source.testpackage1'],
         download_url='https://github.com/elizabethratterree/testpackage',
     )
